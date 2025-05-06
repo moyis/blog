9b14e5bb84edc6a033bdd7c20c5ecdcf444f9d85
Fausto Moya <faustomoya99@gmail.com>
2025-05-06 14:15:06 +0200

    feat: add Open Graph image generation and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,34 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
+import opengraphImages from "astro-opengraph-images";
 import pagefind from "astro-pagefind";
 import { defineConfig, fontProviders } from "astro/config";
-
-import opengraphImages, { presets } from "astro-opengraph-images";
+import { ogImage } from "./src/OgImage";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog-git-feature-og-images-moyis.vercel.app",
-  integrations: [sitemap(), mdx(), pagefind(), opengraphImages({
-    options:{
-      fonts: [
-        {
-          name: "Geist Sans",
-          weight: 400,
-          style: "normal",
-          data: fs.readFileSync("node_modules/@fontsource/geist-sans/files/geist-sans-latin-400-normal.woff"),
-        }
-      ]
-    },
-    render: presets.brandedLogo
-  })],
+  integrations: [
+    sitemap(),
+    mdx(),
+    pagefind(),
+    opengraphImages({
+      options: {
+        fonts: [
+          {
+            name: "Geist Sans",
+            weight: 400,
+            style: "normal",
+            data: fs.readFileSync(
+              "node_modules/@fontsource/geist-sans/files/geist-sans-latin-400-normal.woff",
+            ),
+          },
+        ],
+      },
+      render: ogImage,
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
