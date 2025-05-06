ecc87d4eb2a769645372e683810a5e15859b4297
Fausto Moya <37676075+moyis@users.noreply.github.com>
2025-05-06 20:11:37 +0200

    feature: add astro-opengraph-images (#117)
    * feature: add astro-opengraph-images
    
    * feature: integrate astro-opengraph-images for enhanced metadata handling
    
    * feature: add custom og image
    
    * feature: add @types/react to dependencies and update bun.lock
    
    * fix: improve 404 page layout for better user experience
    
    * fix: update react version to exact match in package.json
    
    ---------
    
    Co-authored-by: Fausto Moya <faustomoya99@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,33 @@ import tailwindcss from "@tailwindcss/vite";
 import pagefind from "astro-pagefind";
 import { defineConfig, fontProviders } from "astro/config";
 
+import opengraphImages from "astro-opengraph-images";
+import { ogImage } from "./src/components/OgImage";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://moyis.dev",
-  integrations: [sitemap(), mdx(), pagefind()],
+  integrations: [
+    sitemap(),
+    mdx(),
+    pagefind(),
+    opengraphImages({
+      options: {
+        fonts: [
+          {
+            name: "Roboto",
+            weight: 400,
+            style: "normal",
+            data: fs.readFileSync(
+              "node_modules/@fontsource/geist-sans/files/geist-sans-latin-400-normal.woff",
+            ),
+          },
+        ],
+      },
+      render: ogImage,
+    }),
+    ,
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
