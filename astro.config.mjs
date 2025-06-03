85801fc9dfdb65e2224a694fa746aa0508e84756
Fausto Moya <faustomoya99@gmail.com>
2025-06-03 23:38:42 +0200

    Revert "feature: use astro font experimental (#104)"
    This reverts commit 51cb6d06e5f71b42241be4a7826a278a4f4c6935.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
-import pagefind from "astro-pagefind";
-import { defineConfig, fontProviders } from "astro/config";
-
 import opengraphImages from "astro-opengraph-images";
+import pagefind from "astro-pagefind";
+import { defineConfig } from "astro/config";
 import { ogImage } from "./src/components/OgImage";
 
 // https://astro.build/config
@@ -38,40 +37,5 @@ export default defineConfig({
     shikiConfig: {
       theme: "css-variables",
     },
-  },
-  experimental: {
-    fonts: [
-      {
-        provider: fontProviders.google(),
-        name: "Geist Sans",
-        cssVariable: "--font-blog",
-        weights: ["400", "500", "600"],
-        fallbacks: [
-          "ui-sans-serif",
-          "system-ui",
-          "sans-serif",
-          "Apple Color Emoji",
-          "Segoe UI Emoji",
-          "Segoe UI Symbol",
-          "Noto Color Emoji",
-        ],
-      },
-      {
-        provider: fontProviders.google(),
-        name: "Geist Mono",
-        cssVariable: "--font-code",
-        weights: ["400", "500", "600"],
-        fallbacks: [
-          "ui-monospace",
-          "SFMono-Regular",
-          "Menlo",
-          "Monaco",
-          "Consolas",
-          "Liberation Mono",
-          "Courier New",
-          "monospace",
-        ],
-      },
-    ],
   },
 });
